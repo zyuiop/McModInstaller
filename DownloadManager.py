0927986fa003b326753b04c514a3b9b25fcc74b4
Gnomino <gnomino@gGentooAmd64.gnet>
2014-02-01 19:58:51 +0100

    Added dependencies support

--- DownloadManager.py
+++ DownloadManager.py
@@ -6,6 +6,7 @@ import Remote
 class DownloadManager:
     def __init__(self, parent):
         self.parent = parent
+        self.dependencies = []
     
     def appendConsole(self, text, delete=False):
         self.parent.logs.config(state=NORMAL)
@@ -22,21 +23,21 @@ class DownloadManager:
             vanilladir = self.parent.remote.mcpath + "/versions/" + pkg['version']
             if os.path.isdir(vanilladir):
                 if os.path.exists(vanilladir + "/" + pkg['version'] + ".jar"):
-                    profilepath = self.parent.remote.mcpath + "/versions/" + pkg['profilename'])
-                    if not os.path.isdir(profilepath)
+                    profilepath = self.parent.remote.mcpath + "/versions/" + pkg['profilename']
+                    if not os.path.isdir(profilepath):
                         mkpath(profilepath)
                     else:
                         self.appendConsole("/!\\ Le dossier " + profilepath + " existe.")
                     if "jarfile" in pkg.keys():
                         self.appendConsole("Téléchargement du minecraft.jar")
-                        if !self.parent.remote.downloadFile(self.parent.remote, 'http://' + self.parent.remote.repo + self.parent.remote.directory + pkg['jarfile'], profilepath + "/" + pkg['profilename'] + '.jar', "Téléchargé : {} / {}", "Téléchargement du minecraft.jar"):
+                        if not self.parent.remote.downloadFile(self.parent.remote, 'http://' + self.parent.remote.repo + self.parent.remote.directory + pkg['jarfile'], profilepath + "/" + pkg['profilename'] + '.jar', "Téléchargé : {} / {}", "Téléchargement du minecraft.jar"):
                             self.appendConsole("Échec lors du téléchargement")
                     else:
                         self.appendConsole("Copie du minecraft.jar")
                         shutil.copy(vanilladir + "/" + pkg['version'] + ".jar", profilepath + "/" + pkg['profilename'] + ".jar")
                         self.appendConsole("Copie terminée") # Comme notre relation, salaud !
                     self.appendConsole("Téléchargement du profil")
-                    if !self.parent.remote.downloadFile(self.parent.remote, 'http://' + self.parent.remote.repo + self.parent.remote.directory + pkg['json'], profilepath + "/" + pkg['profilename'] + ".json"):                        self.appendConsole("Échec lors du téléchargement du profil")
+                    if not self.parent.remote.downloadFile(self.parent.remote, 'http://' + self.parent.remote.repo + self.parent.remote.directory + pkg['json'], profilepath + "/" + pkg['profilename'] + ".json"):                        self.appendConsole("Échec lors du téléchargement du profil")
             else:
                 self.appendConsole("La version " + pkg['version'] + " de Minecraft n'est pas installée.")        
         else:
@@ -45,13 +46,18 @@ class DownloadManager:
                 path += "coremods"
             else:
                 path += "mods"
-            if !os.path.isdir(path):
+            if not os.path.isdir(path):
                 mkpath(path)
             path += "/" + pkg['package_name'] + ".jar"
             i = 0
             worked = false
-            while !worked and i < len(pkg['mirrors']):
+            while not worked and i < len(pkg['mirrors']):
                 worked = self.parent.remote.downloadFile(self.parent.remote, 'http://' + pkg['mirrors'][i]['server'] + pkg['mirrors'][i]['path'], path, "Téléchargé : {} / {}", "Téléchargement de " + pkg['name'])
                 i += 1
-
-
+            self.dependencies.append(pkg['name'])
+            for d in pkg['dependencies']:
+                if not d['name'] in self.dependencies:
+                    self.dependencies.append(d['name'])
+                    dpkg = self.parent.remote.downloadPkgInfo(self.parent.remote, d['name'])
+                    self.download(self, dpkg)
+            
